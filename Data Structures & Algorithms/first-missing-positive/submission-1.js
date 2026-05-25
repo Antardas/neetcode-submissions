39351f9457b9b8a8d5a833ad055c329fad9e7906
Antar Das <antardas2334@gmail.com>
2026-05-25 22:52:14 +0600

    Add: first-missing-positive - submission-1

---
+++ Data Structures & Algorithms/first-missing-positive/submission-1.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    firstMissingPositive(nums) {
+        const numMap = new Set();
+
+        for (let i = 0; i < nums.length; i++){
+            if(nums[i] < 0) continue;
+            numMap.add(nums[i])
+        }
+
+        for (let i = 1; i <= nums.length+1; i++){
+            if(!numMap.has(i)) {
+                return i;
+            }
+        }
+    }
+}
